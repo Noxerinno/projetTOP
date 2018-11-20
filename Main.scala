815bcd52ef8912f1f435da487a3b7b16e31b408e
Noxerino <theocouran3@gmail.com>
2018-11-20 20:28:43 +0100

    Début du parseur

--- Main.scala
+++ Main.scala
@@ -1,13 +1,27 @@
-object Main extends App{
-    val airportValues = (0:Int, null:String, null:String, null:String, 0:Double, 0:Double);
-    var airports = Array(airportValues);
-    var filename : String = null;
-    
-    println(airports.size);
-    airports = airports.tail;
-    println(airports.size);
+import java.nio.charset.CodingErrorAction;
 
-    //def loadAirports(filename:String) : List(airportValues) = {
-    //
-    //}
+import scala.io.Codec;
+import scala.io.Source;
+
+object Main extends App{
+    var filename : String = "src/airports.dat";
+
+    def loadAirports(filename:String) : List[(Int, String, String, String, Double, Double)] = {
+        //Parse les données de airports.dat dans une liste de liste a tuple
+
+        implicit val codec = Codec("UTF-8");
+        codec.onUnmappableCharacter(CodingErrorAction.IGNORE);
+        var airports : List[(Int, String, String, String, Double, Double)] = Nil;       
+        
+        for (line <- Source.fromFile("src/airports.dat").getLines){
+            val cols = line.split(",").map(_.trim);
+            //println(s"${cols(0)}~${cols(1)}~${cols(2)}~${cols(3)}~${cols(6)}~${cols(7)}");
+            airports = (s"${cols(0)}".toInt, s"${cols(1)}", s"${cols(2)}", s"${cols(3)}", s"${cols(6)}".toDouble, s"${cols(7)}".toDouble) :: airports;
+        }
+
+        println(airports.length);
+        return airports;
+    }
+
+    var airports : List[(Int, String, String, String, Double, Double)] = loadAirports(filename);
 }
---
+++ Test.scala
@@ -0,0 +1,9 @@
+import scala.io.Source;
+
+object Test extends App{
+    for (line <- Source.fromFile("src/airports.dat").getLines){
+        val cols = line.split(",").map(_.trim);
+        //println(s"${cols(0)}~${cols(1)}~${cols(2)}~${cols(3)}~${cols(6)}~${cols(7)}~");
+        println(s"cols(0)~cols(1)~cols(2)~cols(3)~cols(6)~cols(7)");
+    }
+}
